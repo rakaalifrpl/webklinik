a20a8d44c98dc381aa4e42369964907ec53f87de
rakaalifrpl <rakapplg@gmail.com>
2024-10-23 09:29:17 +0700

    first commit

---
+++ php1_halamanweb.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Web klinik</title>
+    <link rel="stylesheet" href="php32_style.css">
+</head>
+
+<style type="text/css">
+
+
+</style>
+<body>
+
+<div class="nav">
+
+    <a href=""> Home </a>
+    <a href=""> Profil Kami </a>
+    <a href=""> Galeri  </a>
+    <a href=""> Kntak </a>
+   
+
+
+
+
+</div>    
+<section id="slider">
+<strong>slider</strong>
+<!-- Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias optio incidunt amet id vitae quibusdam? -->
+</section>
+
+<section id="sejarah">
+<strong>sejarah</strong>
+Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere, natus. Architecto necessitatibus deserunt nam omnis!
+</section>
+
+
+<footer>
+    <div class="footer"> 
+<strong>footer</strong>
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus a esse tempore perspiciatis eius nemo.
+
+    </div>
+</footer>
+
+</body>
+</html>
